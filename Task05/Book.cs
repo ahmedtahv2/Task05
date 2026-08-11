1cb97926fefbaf4e88d63370e6da54e8efc9563a
Ahmed_Taha <154711462+ahmedtahv1@users.noreply.github.com>
2026-08-11 11:50:31 +0300

    Q1,Q2,Q3

---
+++ Task05/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task05
+{
+    internal class Book
+    {
+        string password = "secret";
+       internal int copiesInStock = 5;
+        public string Title;
+    }
+}
--- Task05/Program.cs
+++ Task05/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Book book;
+            //Console.WriteLine(book.password);
+            ////we cant reach it because it's private we can only access it from the class 
+            #endregion
+
+            #region Q2
+            //Book book = new Book();
+            //Console.WriteLine(book.copiesInStock);
+            ////we can access it because we can access internal when we are in the same 
+            #endregion project
+
+            #region Q3
+            //Book book = new Book();
+            //book.Title = "Test";
+            //Console.WriteLine(book.Title); 
+            #endregion
+
+
         }
     }
 }
